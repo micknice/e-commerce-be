deb629fa8dfcdcffbe3f02b9f638645bfe7844bd
micknice <mikemccullagh@outlook.com>
2023-10-13 04:50:30 +0100

    session 10 for rollback

--- src/main/java/com/ecommerce_be/ecommerce_backend/api/model/LoginResponse.java
+++ src/main/java/com/ecommerce_be/ecommerce_backend/api/model/LoginResponse.java
@@ -3,6 +3,25 @@ package com.ecommerce_be.ecommerce_backend.api.model;
 public class LoginResponse {
 
     private String jwt;
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getFailureReason() {
+        return failureReason;
+    }
+
+    public void setFailureReason(String failureReason) {
+        this.failureReason = failureReason;
+    }
+
+    private String failureReason;
 
     public String getJwt() {
         return jwt;
