3c780b5753b52af9047066a0124b94f0f7b7d1a9
micknice <mikemccullagh@outlook.com>
2023-10-18 03:40:23 +0100

    fixed a bug where users could log in with externally generated JWT, adds tests for same

--- src/test/java/com/ecommerce_be/ecommerce_backend/service/JWTServiceTest.java
+++ src/test/java/com/ecommerce_be/ecommerce_backend/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.ecommerce_be.ecommerce_backend.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.ecommerce_be.ecommerce_backend.model.LocalUser;
 import com.ecommerce_be.ecommerce_backend.model.dao.LocalUserDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,6 +27,10 @@ public class JWTServiceTest {
     @Autowired
     private LocalUserDAO localUserDAO;
 
+    @Autowired
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
+
 
     @Test
 
@@ -36,6 +45,21 @@ public class JWTServiceTest {
         LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
         String token = jwtService.generateJWT(user);
         Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for auth should contain users username.");
+    }
+
+    @Test
+    public void testJWTNotGeneratedByUs() {
+        String token = JWT.create().withClaim("USERNAME", "UserA")
+                    .sign(Algorithm.HMAC256("InvalidSecret"));
+        Assertions.assertThrows(SignatureVerificationException.class,
+                () -> jwtService.getUsername(token));
+    }
+
+    @Test
+    public void testJWTCorrectlySignedNoIssuer() {
+        String token = JWT.create().withClaim("USERNAME", "UserA")
+                .sign(Algorithm.HMAC256(algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class, () -> jwtService.getUsername(token));
     }
 
 }
