d9da7a1aab250da198ad943fbebfc5fdee107f7e
micknice <mikemccullagh@outlook.com>
2023-10-21 13:51:58 +0100

    adds authentication/verificatioon to socket connection req

--- src/main/java/com/ecommerce_be/ecommerce_backend/api/security/WebSocketConfiguration.java
+++ src/main/java/com/ecommerce_be/ecommerce_backend/api/security/WebSocketConfiguration.java
@@ -1,19 +1,29 @@
 package com.ecommerce_be.ecommerce_backend.api.security;
 
+import com.ecommerce_be.ecommerce_backend.model.LocalUser;
+import com.ecommerce_be.ecommerce_backend.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.authorization.AuthorizationEventPublisher;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocket
@@ -23,9 +33,14 @@ public class WebSocketConfiguration  implements WebSocketMessageBrokerConfigurer
     private ApplicationContext context;
     private JWTRequestFilter jwtRequestFilter;
 
-    public WebSocketConfiguration(ApplicationContext context, JWTRequestFilter jwtRequestFilter) {
+    private static final AntPathMatcher MATCHER = new AntPathMatcher();
+
+    private UserService userService;
+
+    public WebSocketConfiguration(ApplicationContext context, JWTRequestFilter jwtRequestFilter, UserService userService) {
         this.context = context;
         this.jwtRequestFilter = jwtRequestFilter;
+        this.userService = userService;
     }
 
     @Override
@@ -57,6 +72,58 @@ public class WebSocketConfiguration  implements WebSocketMessageBrokerConfigurer
         AuthorizationEventPublisher publisher =
                 new SpringAuthorizationEventPublisher(context);
         authInterceptor.setAuthorizationEventPublisher(publisher);
-        registration.interceptors(jwtRequestFilter ,authInterceptor);
+        registration.interceptors(jwtRequestFilter ,authInterceptor,
+                new RejectClientMessagesOnChannelsChannelInterceptor(),
+                new DestinationLevelAuthorizationChannelInterceptor());
+    }
+
+
+
+    private class RejectClientMessagesOnChannelsChannelInterceptor implements ChannelInterceptor {
+
+        private String[] paths = new String[] {
+                "/topic/user/*/address"
+        };
+
+        @Override
+        public Message<?> preSend(Message<?> message, MessageChannel channel) {
+            if (message.getHeaders().get("simpMessageType").equals(SimpMessageType.MESSAGE)) {
+                String destination = (String) message.getHeaders().get("simpDestination");
+                for (String path: paths) {
+                    if (MATCHER.match(path, destination))
+                        message = null;
+                }
+            }
+            return message;
+        }
+
+
+    }
+
+    private class DestinationLevelAuthorizationChannelInterceptor
+        implements ChannelInterceptor {
+        @Override
+        public Message<?> preSend(Message<?> message, MessageChannel channel) {
+            if (message.getHeaders().get("simpleMessageType").equals(SimpMessageType.SUBSCRIBE)) {
+                String destination = (String) message.getHeaders().get("simpDestination");
+                Map<String, String> params = MATCHER.extractUriTemplateVariables("/topic/user/{userId}/**", destination);
+                try {
+                    Long userId = Long.valueOf(params.get("userId"));
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    if (authentication != null) {
+                        LocalUser user = (LocalUser) authentication.getPrincipal();
+                        if (!userService.userHasPermissionToUser(user, userId)) {
+                           message = null;
+                        }
+                    } else {
+                        message = null;
+                    }
+
+                } catch (NumberFormatException ex){
+                    message = null;
+                }
+            }
+            return message;
+        }
     }
 }
