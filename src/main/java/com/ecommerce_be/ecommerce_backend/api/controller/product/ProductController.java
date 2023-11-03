03f229d02dc996ad3d23b4ad8e39e014e48224f7
micknice <mikemccullagh@outlook.com>
2023-11-03 02:02:52 +0000

    adds getProductByProductId

--- src/main/java/com/ecommerce_be/ecommerce_backend/api/controller/product/ProductController.java
+++ src/main/java/com/ecommerce_be/ecommerce_backend/api/controller/product/ProductController.java
@@ -2,15 +2,14 @@ package com.ecommerce_be.ecommerce_backend.api.controller.product;
 
 import com.ecommerce_be.ecommerce_backend.model.Product;
 import com.ecommerce_be.ecommerce_backend.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -19,33 +18,38 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-
+    @CrossOrigin(origins="*")
     @GetMapping
     public List<Product> getProducts() {
         return productService.getProducts();
     }
 
+    @CrossOrigin(origins="*")
     @GetMapping("/category/{category}")
     public List<Product> getProductsByCategory(@PathVariable String category) {
         List<Product> allProducts  = this.getProducts();
-
         List<Product> filteredProducts = allProducts
                 .stream()
                 .filter(product -> category.equals(product.getCategory()))
                 .collect(Collectors.toList());
-
         return filteredProducts;
-    }@GetMapping("/subCategory/{subCategory}")
+
+    }
+
+    @CrossOrigin(origins="*")
+    @GetMapping("/subCategory/{subCategory}")
     public List<Product> getProductsBySubCategory(@PathVariable String subCategory) {
         List<Product> allProducts  = this.getProducts();
-
-
-
         List<Product> filteredBySubCategoryProducts = allProducts
                 .stream()
                 .filter(product -> subCategory.equals(product.getSub_category()))
                 .collect(Collectors.toList());
-
         return filteredBySubCategoryProducts;
+    }
+    @CrossOrigin(origins="*")
+    @GetMapping("/id/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product  = productService.getProductByProductId(id);
+        return product;
     }
 }
