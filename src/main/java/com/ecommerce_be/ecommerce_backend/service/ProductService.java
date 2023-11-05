34060ab9172c012c8c851b99123f752a24fe303a
micknice <mikemccullagh@outlook.com>
2023-11-05 04:34:11 +0000

    adds sortBy, orderBy and pagination functionality to category/category and category/category/subcategory/subcategory endpoints. tested functioning as expected. TODO: error handling required for sadpath queries

--- src/main/java/com/ecommerce_be/ecommerce_backend/service/ProductService.java
+++ src/main/java/com/ecommerce_be/ecommerce_backend/service/ProductService.java
@@ -4,8 +4,10 @@ import com.ecommerce_be.ecommerce_backend.model.Product;
 import com.ecommerce_be.ecommerce_backend.model.dao.ProductDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -16,6 +18,70 @@ public class ProductService {
     }
     public List<Product> getProducts() {
         return productDAO.findAll();
+    }
+
+    public List<Product> getPaginatedProductsByCategory(String category, int page, int items, String sortBy, String orderBy) {
+        List<Product> allProducts = productDAO.findAll();
+
+        // filter by category
+        List<Product> filteredProducts = allProducts.stream()
+                .filter(product -> product.getCategory().equals(category))
+                .collect(Collectors.toList());
+
+        // sort by sortBy param
+        Comparator<Product> comparator = null;
+        if ("id".equals(sortBy)) {
+            comparator = Comparator.comparing(Product::getId);
+        } else if ("name".equals(sortBy)) {
+            comparator = Comparator.comparing(Product::getName);
+        } else if ("price".equals(sortBy)) {
+            comparator = Comparator.comparing(Product::getPrice);
+        }
+        //reverse order if orderBy="desc"
+        if (comparator != null) {
+            if ("desc".equals(orderBy)) {
+                comparator = comparator.reversed();
+            }
+            filteredProducts.sort(comparator);
+        }
+
+        // calculate  start and end index for pagination
+        int startIndex = page * items;
+        int endIndex = Math.min(startIndex + items, filteredProducts.size());
+
+        return filteredProducts.subList(startIndex, endIndex);
+    }
+
+    public List<Product> getPaginatedProductsBySubCategory(String subCategory, int page, int items, String sortBy, String orderBy) {
+        List<Product> allProducts = productDAO.findAll();
+
+        // filter by category
+        List<Product> filteredProducts = allProducts.stream()
+                .filter(product -> product.getSub_category().equals(subCategory))
+                .collect(Collectors.toList());
+
+        // sort by sortBy param
+        Comparator<Product> comparator = null;
+        if ("id".equals(sortBy)) {
+            comparator = Comparator.comparing(Product::getId);
+        } else if ("name".equals(sortBy)) {
+            comparator = Comparator.comparing(Product::getName);
+        } else if ("price".equals(sortBy)) {
+            comparator = Comparator.comparing(Product::getPrice);
+        }
+        //reverse order if orderBy="desc"
+        if (comparator != null) {
+            if ("desc".equals(orderBy)) {
+                comparator = comparator.reversed();
+            }
+            filteredProducts.sort(comparator);
+        }
+
+        // calculate  start and end index for pagination
+        int startIndex = page * items;
+        int endIndex = Math.min(startIndex + items, filteredProducts.size());
+
+        return filteredProducts.subList(startIndex, endIndex);
     }
 
     public Optional<Product> getProductByProductId(Long productId) {
